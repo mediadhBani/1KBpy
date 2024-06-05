1a7671c7890faac1eba3e26a040994e6f523f087
Mohamed-iadh Bani <79471859+mediadhBani@users.noreply.github.com>
2024-06-05 20:24:06 +0200

    Refonte du code complète
    - ajout de logiques d'intéractions joueur-carte dans la classe Player
    - ajout de couleurs à l'affichage
    - ajout de règles supplémentaires (rejouer après avoir posé une botte)
    
    bogues connus :
    
    - le compteur de carte Bornes restantes est en retard sur la quantité
      réelle de cartes Bornes dans la pioche amenant à piocher dans un
      conteneur vide (`[].pop()`)

--- milleBornes.py
+++ milleBornes.py
@@ -1,9 +1,14 @@
 # pyright: strict
 
 from abc import ABC, abstractmethod
-from enum import IntFlag, auto
+from enum import IntEnum, IntFlag, auto
 from dataclasses import dataclass, field
-from typing import Self
+from typing import Iterable, Self, Any
+
+class Rule(IntEnum):
+    WINNING_DISTANCE: int = 1000
+    SPEED_LIMIT = 50
+    MAX_USE_200 = 2
 
 class State(IntFlag):
     RED_LIGHT = auto()
@@ -18,32 +23,46 @@ class State(IntFlag):
     
 
 @dataclass(frozen=True, slots=True)
-class Card[T: (int, State)](ABC):
-    value: T
+class Card(ABC):
+    value: Any
     
-    def __rmul__(self, other: int) -> list[Self]:
-        """Fonction d'appoint pour dupliquer une carte plusieurs fois"""
-        return [self for _ in range(other)]
-
+    @classmethod
     @abstractmethod
-    def __post_init__(self):
+    def get_content(cls) -> dict[Any, int]:
+        """Renvoie un dictionnaire de cartes de la classe courante et leur nombre dans le jeu."""
         pass
 
-class Distance(Card[int]):
-    ALLOWED_VALUES: list[int] = [25, 50, 75, 100, 200]
-    SPEED_LIMIT: int = 50
+    @classmethod
+    def generate_all(cls) -> list[Self]:
+        cards: list[Self] = []
+        return sum((v * [cls(k)] for k, v in cls.get_content().items()), cards)
 
-    def __post_init__(self):
-        assert type(self.value) is int and self.value in self.ALLOWED_VALUES, \
-            f"Mauvaise initialisation de la carte Bornes. Valeur reçue : {self.value!r}"
+class Distance(Card):
+    @classmethod
+    def get_content(cls) -> dict[int, int]:
+        return {
+        25: 10,
+        50: 10,
+        75: 10,
+        100: 12,
+        200: 4,
+    }
 
     def __str__(self) -> str:
         return f"{self.value} bornes"
         
-class Hazard(Card[State]):
-    def __post_init__(self):
-        assert type(self.value) is State and self.value.is_singular(), \
-            f"Mauvaise initialisation de la carte Attaque. Valeur reçue : {self.value!r}"
+class Hazard(Card):
+    value: State
+
+    @classmethod
+    def get_content(cls) -> dict[State, int]:
+        return {
+            State.RED_LIGHT: 5,
+            State.SPEED_LIMIT: 4,
+            State.OUT_OF_FUEL: 3,
+            State.FLAT_TIRE: 3,
+            State.ACCIDENT: 3,
+        }
 
     def __str__(self) -> str:
         match self.value:
@@ -55,10 +74,18 @@ class Hazard(Card[State]):
 
         raise ValueError(f"Carte Corrompue ! Valeur reçue : {self.value!r}")
     
-class Remedy(Card[State]):
-    def __post_init__(self):
-        assert type(self.value) is State and self.value.is_singular(), \
-            f"Mauvaise initialisation de la carte Parade. Valeur reçue : {self.value!r}"
+class Remedy(Card):
+    value: State
+
+    @classmethod
+    def get_content(cls) -> dict[State, int]:
+        return {
+            State.RED_LIGHT: 14,
+            State.SPEED_LIMIT: 6,
+            State.OUT_OF_FUEL: 6,
+            State.FLAT_TIRE: 6,
+            State.ACCIDENT: 6,
+        }       
 
     def __str__(self) -> str:
         match self.value:
@@ -70,23 +97,27 @@ class Remedy(Card[State]):
 
         raise ValueError(f"Carte Corrompue ! Valeur reçue : {self.value!r}")
 
-class Safety(Card[State]):
-    ALLOWED_VALUES: list[State] = [State(s) for s in [1|2, 4, 8, 16]]
+class Safety(Card):
+    value: State
 
-    def __post_init__(self):
-        assert type(self.value) is State and self.value in self.ALLOWED_VALUES, \
-            f"Mauvaise initialisation de la carte Botte. Valeur reçue : {self.value!r}"
+    @classmethod
+    def get_content(cls) -> dict[State, int]:
+        return {
+            State.RED_LIGHT | State.SPEED_LIMIT: 1,
+            State.OUT_OF_FUEL: 1,
+            State.FLAT_TIRE: 1,
+            State.ACCIDENT: 1,
+        }       
 
     def __str__(self) -> str:
-        if self.value == State.RED_LIGHT | State.SPEED_LIMIT:
-            return "Véhicule prioritaire"
-
         match self.value:
+            case State.RED_LIGHT | State.SPEED_LIMIT: return "Véhicule prioritaire"
             case State.OUT_OF_FUEL: return "Citerne d'essence"
             case State.FLAT_TIRE: return "Increvable"
             case State.ACCIDENT: return "As du volant"
-            case _:
-                raise ValueError(f"Carte Corrompue ! Valeur reçue : {self.value!r}")
+
+        if self.value is State.RED_LIGHT | State.SPEED_LIMIT:
+            return "Véhicule prioritaire"
 
 @dataclass
 class Player:
@@ -94,17 +125,31 @@ class Player:
     count200: int = 0
     hazards: State = State(0)
     safeties: State = State(0)
-    hand: list[Card[int | State]] = field(default_factory=list)
+    hand: list[Card] = field(default_factory=list)
 
-    def is_slow(self) -> bool:
-        return State.SPEED_LIMIT in self.hazards
+    def can_get(self, card: Card) -> bool:
+        """Vérifier si le joueur courant peut recevoir les effets de la carte considérée."""
+        match card:
+            case Safety():
+                return True
+            case Remedy():
+                return self.hazards & card.value is not State(0)
+            case Distance():
+                return self.hazards & ~State.SPEED_LIMIT is State(0) \
+                    and (card.value <= Rule.SPEED_LIMIT or not State.SPEED_LIMIT in self.hazards) \
+                    and self.score + card.value <= Rule.WINNING_DISTANCE \
+                    and (card.value != 200 or self.count200 < Rule.MAX_USE_200)
+            case Hazard():
+                if card.value is State.SPEED_LIMIT:
+                    return State.SPEED_LIMIT not in self.hazards | self.safeties
+                return self.hazards & ~State.SPEED_LIMIT is State(0) and card.value not in self.safeties
+            case _:
+                raise NotImplemented
 
-
-
-def validate_input(prompt: str, upperbound: int) -> int:
+def validate_input(prompt: str, allowed_values: Iterable[int]) -> int:
     while True:
         try:
-            if 0 <= (value := int(input(prompt))) < upperbound:
+            if (value := int(input(prompt))) in allowed_values:
                 return value
             raise ValueError
         except ValueError:
@@ -113,95 +158,116 @@ def validate_input(prompt: str, upperbound: int) -> int:
     
 if __name__ == "__main__":
     from random import shuffle
-    type Card_ = Card[int | State]
 
-    deck: list[Card_] = [Safety(State(s)) for s in Safety.ALLOWED_VALUES]
-    
-    for n, s in zip([5, 4, 3, 3, 3], State):
-        deck.extend(n * Hazard(s))
-
-    for n, s in zip([14, 6, 6, 6, 6], State):
-        deck.extend(n * Remedy(s))
-
-    for n, d in zip([10, 10, 10, 12, 4], Distance.ALLOWED_VALUES):
-        deck.extend(n * Distance(d))
-
+    # instanciation de la pioche
+    deck: list[Card] = []
+    for card_class in [Distance, Hazard, Remedy, Safety]:
+        deck.extend(card_class.generate_all())
     shuffle(deck)
 
+    # instanciation des joueurs
     number_players: int = 2
     players = [Player(hazards=State.RED_LIGHT) for _ in range(number_players)]
+    for player in players:
+        player.hand = [deck.pop() for _ in range(6)]
 
-    for i, p in enumerate(players):
-        p.hand = [deck.pop() for _ in range(6)]
+    # compteurs et drapeau
+    remaining_distances: int = sum(type(card) is Distance for card in deck)
+    turns: int = 0
+    turn_end = True
+    player = players[0]
 
-
-    DISTANCE_WIN: int = 1000
-    remaining_distances: int = 46
-    turns = 0
+    def card_color(card: Card):
+        match card:
+            case Hazard(): return "\x1B[31m"
+            case Remedy(): return "\x1B[34m"
+            case Safety(): return "\x1B[32m"
+            case _: return "\x1B[0m"
 
     while True:
-        card, player = deck.pop(), players[turns % number_players]
-        remaining_distances -= type(card) is Distance
+        if turn_end:
+            player = players[turns % number_players]
+            card = deck.pop()
+            player.hand.append(card)
+            remaining_distances -= type(card) is Distance
+            turn_end = False
 
-        # montrer main en jeu
-        player.hand.append(card)
-        print("\n".join(f"{i}: {c}" for i, c in enumerate(player.hand)))
-
-        # montrer stats
-        print(f"{player.score}/{DISTANCE_WIN} bornes", end=" | ")
+        # montrer l'interface du jeu
+        print()
+        for i, c in enumerate(player.hand):
+            print(f"{i}: {card_color(c)}{c}\x1B[m")
+        print("J", turns % number_players, sep="", end=" | ")                     # joueur courant
+        print(f"{player.score}/{Rule.WINNING_DISTANCE} bornes", end="\x1B[m | ")  # distance parcourue
         if player.safeties:
             print("\x1B[32m", end="")
-            print(*map(Safety, player.safeties), sep=", ", end="\x1B[m | ")
+            print(*map(Safety, player.safeties & ~State.SPEED_LIMIT), sep=", ", end="\x1B[m | ")       # bottes
         if player.hazards:
             print("\x1B[31m", end="")
-            print(*map(Hazard, player.hazards), sep=", ", end="\x1B[m\n")
+            print(*map(Hazard, player.hazards), sep=", ", end="\x1B[m\n")         # attaques subies
         else:
             print("La voie est libre")
 
+        # valider la saisie
+        if (choice := input("[q = quitter] [n = jouer carte n] [-n = retirer carte n] ")) \
+            not in {"q", "-0", "-1", "-2", "-3", "-4", "-5", "-6", "0", "1", "2", "3", "4", "5", "6"}:
+            print("Saisie invalide")
+            continue
 
-        card_idx = validate_input("Jouer une carte en saisissant l'indice correspondant : ", 7)
-        match (card := player.hand[card_idx]):
-            case Distance():
-                if (not player.hazards or player.hazards is State.SPEED_LIMIT and card.value <= Distance.SPEED_LIMIT) \
-                and player.score + card.value <= DISTANCE_WIN \
-                and player.count200 < 2:
-                    player.score += card.value
-                    player.count200 += (card.value == 200)
-                else:
-                    print("Vous ne pouvez pas jouer cette carte.")
+        # quitter la partie si "q"
+        if choice == "q":
+            break
+
+        # aplliquer les effets
+        card_idx = abs(int(choice))
+        if choice[0] != "-":
+            card = player.hand[card_idx]
+
+            if type(card) is Hazard:
+                if (choice := input("[r = retour] [n = cibler joueur n] ")) == "r":
                     continue
+                elif choice not in set(map(str, range(number_players))) - set(str(turns % number_players)):
+                    print("Saisie invalide")
+                    continue
+                target = players[int(choice)]
+            else:
+                target = player
 
-            case Hazard():
-                target_idx = validate_input("Quel joueur attaquer ?", len(players))
-
-                if not (target := players[target_idx]) is player \
-                and not (target.is_slow() and card.value is State.SPEED_LIMIT) \
-                and (target.hazards | target.safeties) & ~State.SPEED_LIMIT is State(0):
-                    # attaque validée, mais il faut dorénavant prendre en compte un éventuel coup-fourré
-                    if any(card.value in c.value for c in target.hand if type(c) is Safety):
-                        print("coup fourré !")
-                        turn = target_idx-1
+            if not target.can_get(card):
+                print("Vous ne pouvez pas jouer cette carte.")
+                continue
+            
+            match card:
+                case Distance():
+                    target.score += card.value
+                    target.count200 += (card.value == 200)
+                case Remedy():
+                    target.hazards &= ~card.value
+                case Safety():
+                    target.hazards &= ~card.value
+                    target.safeties |= card.value
+                    turns -= 1
+                    print("rejouez.")
+                case Hazard():
+                    for i, c in enumerate(target.hand):
+                        if type(c) is Safety and card.value & c.value != State(0):
+                            print("Coup fourré !")
+                            target.hand.pop(i)
+                            target.safeties |= c.value
+                            card = deck.pop()
+                            remaining_distances -= (type(c) is Distance)
+                            target.hand.append(card)
+                            turns = players.index(target) - 1
+                            break
                     else:
                         target.hazards |= card.value
-                else:
-                    print("Vous ne pouvez pas attaquer dans ces conditions.")
-                    continue
+                case _:
+                    raise NotImplemented
 
-            case Remedy():
-                if player.hazards & card.value:
-                    player.hazards &= ~card.value
-                else:
-                    print("Cette Parade ne change rien à votre situation !")
-                    continue
-
-            case Safety():
-                player.safeties |= card.value
-                player.hazards &= ~card.value
-
+        # la carte jouée est défaussée
         player.hand.pop(card_idx)
 
         # check end of game
-        if player.score == DISTANCE_WIN:
+        if player.score == Rule.WINNING_DISTANCE:
             print(f"Le joueur J{players.index(player)} a gagné !")
             break
 
@@ -211,4 +277,9 @@ if __name__ == "__main__":
             break
 
         turns += 1
+        turn_end = True
+
+    # tableau des scores
+    for rank, player in enumerate(sorted(players, key=lambda p: p.score, reverse=True)):
+        print(f"#{rank+1} J{players.index(player)} {player.score: >4}")
 
